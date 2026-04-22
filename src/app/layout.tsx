e5ce6dd1c66e3f4fc5df2fa45061bba9ca793577
smj <smj123432@gmail.com>
2026-04-22 13:15:56 +0900

    feat: 사이드바 파일 수정 및 루트레이아웃 임폴트 삭제

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from 'react-hot-toast';
 import { queryClient } from '@/src/lib/queryClient';
 import './globals.css';
-import Sidebar from '../components/layout/Sidebar/sidebar';
 
 export default function RootLayout({
   children,
