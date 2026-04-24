8bacea1ecbd65235d2adc91926028ad91d8becc5
myj9713 <myj9713@gmail.com>
2026-04-24 20:01:50 +0900

    [feat] 게스글 작성페이지 테스트를 위한 코드

--- src/components/layout/PageHeader.tsx
+++ src/components/layout/PageHeader.tsx
@@ -34,17 +34,21 @@ export default function Pageheader({
   const router = useRouter();
   const handleWriteClick = async (e: React.MouseEvent) => {
     e.preventDefault();
-    const {
-      data: { user },
-    } = await supabase.auth.getUser();
-    if (!user) {
-      router.push('/login');
-      return;
-    }
-    // 로그인 됐으면 writeLink로 이동
+    // TODO: 로그인 체크 임시 bypass, 체크 완료시 삭제 및 아래 주석 풀기
     if (writeLink) {
       router.push(writeLink);
     }
+    // const {
+    //   data: { user },
+    // } = await supabase.auth.getUser();
+    // if (!user) {
+    //   router.push('/login');
+    //   return;
+    // }
+    // // 로그인 됐으면 writeLink로 이동
+    // if (writeLink) {
+    //   router.push(writeLink);
+    // }
   };
   return (
     <div className="flex flex-col gap-5 bg-white z-10 py-6">
