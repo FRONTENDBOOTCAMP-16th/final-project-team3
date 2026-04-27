970517e6ea464e69747495d5ae777c477a3fc73b
smj <smj123432@gmail.com>
2026-04-27 17:40:12 +0900

    design: 버튼 색상 변경

--- src/app/(main)/community/page.tsx
+++ src/app/(main)/community/page.tsx
@@ -2,43 +2,26 @@
 import { useState, useRef, useEffect } from 'react';
 import Pageheader from '@/components/layout/PageHeader';
 import Postcard from '../../../components/community/Postcard';
-// import { dummyPosts } from '@/constants/dummyData';
-import { useInfiniteScroll } from '@//hooks/useInfiniteScroll';
+import { useInfiniteScroll } from '@/hooks/useInfiniteScroll';
 import { useDebounce } from '@/hooks/useDebounce';
 import LoadingSpinner from '@/components/common/LoadingSpinner';
-import { getPosts } from '@/services/communityService';
-import type { Post } from '@/types/community';
+import { usePosts } from '@/hooks/useCommunity';
 
 const PAGE_SIZE = 10;
 
 export default function CommunityPage() {
-  const [isLoading, setIsLoading] = useState(false);
   const [activeTab, setActiveTab] = useState('전체');
   const [searchQuery, setSearchQuery] = useState('');
   const [page, setPage] = useState(1);
   const [headerHeight, setHeaderHeight] = useState(0);
   const headerRef = useRef<HTMLDivElement>(null);
   const debouncedSearch = useDebounce(searchQuery, 300);
-  const [posts, setPosts] = useState<Post[]>([]);
+  const { data: posts = [], isLoading } = usePosts(); // ✅ 교체
 
   useEffect(() => {
     if (headerRef.current) {
       setHeaderHeight(headerRef.current.offsetHeight);
     }
-  }, []);
-
-  useEffect(() => {
-    const load = async () => {
-      try {
-        const data = await getPosts();
-        setPosts(data);
-      } catch (e) {
-        console.error(e);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    load();
   }, []);
 
   const filteredPosts = posts.filter((post) => {
@@ -61,7 +44,6 @@ export default function CommunityPage() {
 
   return (
     <main className="w-full min-h-screen">
-      {/* 헤더 fixed 고정 */}
       <div
         ref={headerRef}
         className="fixed top-0 left-50 right-0 z-10 bg-white shadow-md flex justify-center"
@@ -86,7 +68,6 @@ export default function CommunityPage() {
         </div>
       </div>
 
-      {/* 카드 영역 - 헤더 높이 자동 반영 */}
       <div
         style={{ paddingTop: `${headerHeight + 24}px` }}
         className="pb-6 flex justify-center"
