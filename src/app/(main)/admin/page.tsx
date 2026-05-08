fe53e9bc454b83d578c1d0887c53d203249b37fc
holymolyRon <wjdfhs111@gmail.com>
2026-05-08 16:34:38 +0900

    feat: 대시보드 header 추가

--- src/app/(main)/admin/page.tsx
+++ src/app/(main)/admin/page.tsx
@@ -1,3 +1,29 @@
+import { createServerClient } from '@supabase/ssr';
+import { cookies } from 'next/headers';
+
+import AdminHeader from '@/components/admin/AdminHeader';
+
+// async function getAdminDashboard() {
+//   const cookieStore = await cookies();
+//   const supabase = createServerClient(
+//     process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+//     {
+//       cookies: {
+//         getAll() {
+//           return cookieStore.getAll();
+//         },
+//       },
+//     },
+//   );
+// }
+
 export default function AdminDashboardtPage() {
-  return <main className="w-full min-h-screen space-y-2">대쉬보드</main>;
+  // const postData = await getAdminDashboard();
+
+  return (
+    <main className="w-full min-h-screen space-y-2">
+      <AdminHeader page="dashboard" />
+    </main>
+  );
 }
