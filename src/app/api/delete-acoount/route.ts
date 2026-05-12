debe544a7af995eeaaca34052f427bd66f6a2753
lcmm <lcmbook55@gmail.com>
2026-05-12 14:03:10 +0900

    feat: delete-account API Route 생성

---
+++ src/app/api/delete-acoount/route.ts
@@ -0,0 +1,22 @@
+import { createClient } from '@supabase/supabase-js';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function DELETE(req: NextRequest) {
+  const { userId } = await req.json();
+
+  const supabaseAdmin = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!,
+  );
+
+  const { error } = await supabaseAdmin.auth.admin.deleteUser(userId);
+
+  if (error) {
+    return NextResponse.json(
+      { error: '회원탈퇴에 실패했습니다.' },
+      { status: 500 },
+    );
+  }
+
+  return NextResponse.json({ success: true });
+}
